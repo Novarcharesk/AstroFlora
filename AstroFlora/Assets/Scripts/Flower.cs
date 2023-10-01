a7b93e9e318cdd6b688b349928e34727e02d6b70
Nova Cravey <114209659+Novarcharesk@users.noreply.github.com>
2023-10-01 14:27:56 +1100

    Achieved the spawning of the flowers on either planet

--- AstroFlora/Assets/Scripts/Flower.cs
+++ AstroFlora/Assets/Scripts/Flower.cs
@@ -1,21 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
 
 public class Flower : MonoBehaviour
 {
     public GameObject destinationPlanetPrefab; // Reference to the destination planet prefab
-    public float maxSaturation = 1.0f;
-    public float currentSaturation;
-    private float despawnTimer;
     public float timeToDespawn = 10.0f; // Time in seconds before the flower despawns
     public GameObject planet; // Reference to the planet where this flower resides
-    public GameObject[] flowerPrefabs; // Array to hold different flower prefabs
-
-
-    // Reference to the UI slot where the flower should be placed when collected
-    public Transform uiSlot;
 
     // Variable to track whether the flower has been collected
     private bool isCollected = false;
@@ -29,23 +20,8 @@ public class Flower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Implement any flower-related behaviors here
-        // For example, decrease saturation over time
-        if (!isCollected)
-        {
-            DecreaseSaturation(Time.deltaTime);
-            // Check if the flower has lost all saturation and should despawn
-            if (currentSaturation <= 0)
-            {
-                Despawn();
-            }
-        }
-    }
-
-    public void DecreaseSaturation(float amount)
-    {
-        currentSaturation -= amount;
-        // Update visual indicators here
+        // Implement any flower-related behaviors here (if needed)
+        // You can add any custom behavior or effects here
     }
 
     public void Collect()
@@ -58,11 +34,7 @@ public class Flower : MonoBehaviour
             // Deactivate or destroy the flower (you can choose which option to use)
             gameObject.SetActive(false); // Deactivate the flower
 
-            // Move the flower's visual representation to the UI slot
-            Transform visualFlower = Instantiate(transform, uiSlot.position, UnityEngine.Quaternion.identity);
-            visualFlower.SetParent(uiSlot);
-
-            // You may need to adjust the position, scale, or other properties of the visualFlower
+            // You may need to adjust other behaviors related to flower collection
         }
     }
 
--- AstroFlora/Assets/Scripts/FlowerSpawner.cs
+++ AstroFlora/Assets/Scripts/FlowerSpawner.cs
@@ -7,50 +7,35 @@ public class FlowerSpawner : MonoBehaviour
     public GameObject[] flowerPrefabs; // Array to hold different flower prefabs
     public Transform[] spawners; // Array of spawner Transforms on the planet
 
-    // Other variables for spawn rate, timer, etc. (if needed)
+    public float initialSpawnDelay = 2f; // Initial delay before the first flower spawns
+    public float spawnInterval = 5f; // Time interval between flower spawns
+    public float minSpawnInterval = 1f; // Minimum time interval between spawns
+    public float spawnIntervalDecreaseRate = 0.1f; // Rate at which spawn interval decreases
 
     private void Start()
     {
-        // Initialize any variables or setup logic here
-
-        // Spawn the first flower with a delay (e.g., 2 seconds) after the game starts
-        Invoke("SpawnFirstFlower", 2f);
+        // Initialize variables and setup logic here
+        InvokeRepeating("SpawnFlower", initialSpawnDelay, spawnInterval);
     }
 
-    private void Update()
-    {
-        // Implement any spawn-related logic (e.g., spawn rate) here
-        // You can use Time.deltaTime to track time for spawning flowers
-    }
-
-    // Create a method to spawn the first flower
-    // Create a method to spawn the first flower
-    private void SpawnFirstFlower()
-    {
-        // Choose a random spawner from the array
-        int randomSpawnerIndex = Random.Range(0, spawners.Length);
-        Transform selectedSpawner = spawners[randomSpawnerIndex];
-
-        // Choose a random flower prefab from the array
-        int randomFlowerIndex = Random.Range(0, flowerPrefabs.Length);
-        GameObject selectedFlowerPrefab = flowerPrefabs[randomFlowerIndex];
-
-        // Instantiate the selected flower prefab at the selected spawner's position
-        Instantiate(selectedFlowerPrefab, selectedSpawner.position, Quaternion.identity);
-    }
-
-    // Create a method to spawn a flower on a random spawner
-    public void SpawnFlower()
+    private void SpawnFlower()
     {
         // Choose a random spawner from the array
         int randomSpawnerIndex = Random.Range(0, spawners.Length);
         Transform selectedSpawner = spawners[randomSpawnerIndex].transform;
 
-        // Choose a random flower prefab from the array
-        int randomFlowerIndex = Random.Range(0, flowerPrefabs.Length);
-        GameObject selectedFlowerPrefab = flowerPrefabs[randomFlowerIndex];
+        // Check if there's already a flower at this spawner
+        if (selectedSpawner.childCount == 0)
+        {
+            // Choose a random flower prefab from the array
+            int randomFlowerIndex = Random.Range(0, flowerPrefabs.Length);
+            GameObject selectedFlowerPrefab = flowerPrefabs[randomFlowerIndex];
 
-        // Instantiate the selected flower prefab at the selected spawner's position
-        Instantiate(selectedFlowerPrefab, selectedSpawner.position, Quaternion.identity);
+            // Instantiate the selected flower prefab at the selected spawner's position
+            Instantiate(selectedFlowerPrefab, selectedSpawner.position, Quaternion.identity);
+
+            // Decrease the spawn interval over time to increase frequency
+            spawnInterval = Mathf.Max(minSpawnInterval, spawnInterval - spawnIntervalDecreaseRate);
+        }
     }
 }
